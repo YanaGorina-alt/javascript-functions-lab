2283c2291d9bb5e0cbac26fb6f9215cf84d9f3d8
Yana Gorina <yagorina.ne@gmail.com>
2023-03-07 09:33:44 -0800

    minusOne function

---
+++ functions.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function called minusOne that takes a parameter num.
+ *  Assuming the argument is a number,
+ *  print the argument -1.
+ */
+function minusOne(num){
+    console.log(num-1);
+}
+minusOne(10);        // 9
+minusOne(100);       // 99
+minusOne(Infinity);  // Infinity
